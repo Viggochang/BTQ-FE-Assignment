7dca34bf48e3e68f75105118f652cbf54f07cfc2
Viggochang <gl4cj8686@gmail.com>
2021-12-27 20:54:19 +0800

    Edit activity text style in nav

--- src/components/header/navigation/NavigationItem.js
+++ src/components/header/navigation/NavigationItem.js
@@ -5,7 +5,7 @@ export default function NavigationItem({ data }) {
   return (
     <div className="w-[20.5vw] py-[5px] text-white">
       <div className="text-[12px] text-[#909090] leading-[14px]">{day}</div>
-      <div>{activity}</div>
+      <div className="text-[14px] leading-[18px]">{activity}</div>
     </div>
   );
 }
