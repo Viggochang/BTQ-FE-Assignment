6225bc4e36cc33c8eb3cb12a06f9f7645191d0c7
Viggochang <gl4cj8686@gmail.com>
2021-12-27 20:16:14 +0800

    Create navigation data

--- src/components/header/navigation/index.js
+++ src/components/header/navigation/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import NavigationItem from "./NavigationItem";
 
+const data = [
+  ["MONDAY", "FILM"],
+  ["TUESDAY", "WORKSHOPS"],
+  ["WEDNESDAY", "PRACTICAL"],
+  ["THURSDAY", "CONFERENCE"],
+  ["FRIDAY", "MUSIC"],
+  ["SATURDAY", "FRINGE"],
+];
+
 export default function Navigation() {
   return <nav></nav>;
 }
