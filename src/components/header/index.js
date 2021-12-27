3700c67e88b5cf0848e42acdb53b0efca7f29913
Viggochang <gl4cj8686@gmail.com>
2021-12-27 21:36:22 +0800

    Edit icon in MobileHeader

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import WebHeader from "./webHeader";
+import MobileHeader from "./mobileHeader";
 
 export default function Header() {
   return (
     <>
       <WebHeader />
+      <MobileHeader />
     </>
   );
 }
