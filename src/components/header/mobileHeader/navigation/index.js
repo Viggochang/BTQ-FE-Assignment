0e2f4e88ad161b465ab3a30e5edf94c1379800de
Viggochang <gl4cj8686@gmail.com>
2021-12-27 23:14:21 +0800

    Init navigationItem for mobile

--- src/components/header/mobileHeader/navigation/index.js
+++ src/components/header/mobileHeader/navigation/index.js
@@ -1,6 +1,14 @@
 import React from "react";
+import NavigationItem from "./NavigationItem";
+
 import { navData } from "../../../../data/data";
 
 export default function Navigation() {
-  return <div className="w-full absolute top-[45px] px-[10px]"></div>;
+  return (
+    <div className="w-full absolute top-[45px] px-[10px]">
+      {navData.map((data) => (
+        <NavigationItem data={data} />
+      ))}
+    </div>
+  );
 }
