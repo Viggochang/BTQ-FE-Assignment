c1bd1a24215c16523f4e97222fb18874a7b68a7e
Viggochang <gl4cj8686@gmail.com>
2021-12-28 05:01:42 +0800

    Modify custom class name for section background image

--- src/components/conference/index.js
+++ src/components/conference/index.js
@@ -8,7 +8,7 @@ import Speakers from "./speakers";
 export default function Conference() {
   return (
     <section
-      className="section-background section-bg-image"
+      className="section-background section-bg-image bg-primary"
       style={{
         "background-image":
           "url(https://2012.buildconf.com/images/conf_bg@2x2.png)",
