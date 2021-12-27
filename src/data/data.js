64927c7e4dfc32c2a4b5cd7a3abec4b96f39c9c0
Viggochang <gl4cj8686@gmail.com>
2021-12-28 02:45:58 +0800

    Add speakers data

--- src/data/data.js
+++ src/data/data.js
@@ -12,4 +12,25 @@ export const conferenceData = {
   dateLoc: "November 12-16, 2012 in Belfast, Northern Ireland",
   intro:
     "Build is a festival for people who design for the web. For one week in November, a community of smart, talented people come together to share ideas, tell stories, get their hands dirty, and drink a few beers.",
+  speakers: [
+    { name: "Robin Sloan", img: "https://2012.buildconf.com/images/robin.png" },
+    {
+      name: "Kirby Ferguson",
+      img: "https://2012.buildconf.com/images/kirby.png",
+    },
+    {
+      name: "Rob Giampietro",
+      img: "https://2012.buildconf.com/images/rob.png",
+    },
+    { name: "Mandy Brown", img: "https://2012.buildconf.com/images/mandy.png" },
+    { name: "Jeff Veen", img: "https://2012.buildconf.com/images/jeff.png" },
+    {
+      name: "Tiffani Jones Brown",
+      img: "https://2012.buildconf.com/images/tiffani.png",
+    },
+    {
+      name: "Ethan Marcotte",
+      img: "https://2012.buildconf.com/images/ethan.png",
+    },
+  ],
 };
