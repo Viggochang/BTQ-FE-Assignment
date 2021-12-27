7ad17aea417713a4bfae702f962ede2b6e1e6c6d
Viggochang <gl4cj8686@gmail.com>
2021-12-28 02:51:01 +0800

    Modify home registration margin

--- src/components/conference/HomeRegister.js
+++ src/components/conference/HomeRegister.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function HomeRegister() {
   return (
-    <div className="flex w-[140px] h-[39px] bg-my-black rounded-[7px]">
+    <div className="flex w-[140px] h-[39px] mb-[35px] bg-my-black rounded-[7px]">
       <div className="m-auto text-white text-[12px] font-semibold tracking-[1px] md:text-[11px]">
         SOLD OUT
       </div>
