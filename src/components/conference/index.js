79a081e35dbc774f4f75565e0295197dc797519a
Viggochang <gl4cj8686@gmail.com>
2021-12-28 00:42:40 +0800

    Edit conference style

--- src/components/conference/index.js
+++ src/components/conference/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Conference() {
-  return <section className="w-full h-[300px] px-[4vw] bg-primary"></section>;
+  return <section className="w-full px-[4vw] bg-primary"></section>;
 }
