d35680ff56657e7ddb56450b6c9d3d5bdd08d063
Viggochang <gl4cj8686@gmail.com>
2021-12-28 02:05:06 +0800

    Edit registration button style

--- src/components/conference/HomeRegister.js
+++ src/components/conference/HomeRegister.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function HomeRegister() {
-  return <div></div>;
+  return (
+    <div className="w-[140px] h-[39px] bg-my-black rounded-[7px]">SOLD OUT</div>
+  );
 }
