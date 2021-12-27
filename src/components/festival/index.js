19f6f5f599fa5c67deab02163b4ca8fb23349fd7
Viggochang <gl4cj8686@gmail.com>
2021-12-28 07:11:42 +0800

    Edit festival background style

--- src/components/festival/index.js
+++ src/components/festival/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Festival() {
-  return <div></div>;
+  return (
+    <section className="section-background">
+      <div class="section-container"></div>
+    </section>
+  );
 }
