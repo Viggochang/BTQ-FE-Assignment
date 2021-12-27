391505656dccfbddb232768ff957cb2a995ced3a
Viggochang <gl4cj8686@gmail.com>
2021-12-27 23:37:29 +0800

    Edit nav text

--- src/components/header/mobileHeader/navigation/NavigationItem.js
+++ src/components/header/mobileHeader/navigation/NavigationItem.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-export default function NavigationItem() {
+export default function NavigationItem({ data }) {
+  const [day, activity] = data;
   return (
-    <div className="w-full h-[49px] border-t-[1px] border-[#272727] border-solid"></div>
+    <div className="w-full h-[49px] border-t-[1px] py-[8px] pl-[10px] border-[#272727] border-solid tracking-[1px]">
+      <div className="text-[9px] text-[#909090] leading-[14px]">{day}</div>
+      <div className="text-[12px] text-white leading-[18px]">{activity}</div>
+    </div>
   );
 }
--- src/components/header/webHeader/navigation/NavigationItem.js
+++ src/components/header/webHeader/navigation/NavigationItem.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function NavigationItem({ data }) {
   const [day, activity] = data;
   return (
-    <div className="w-[20.5vw] py-[5px] text-white tracking-[1px] xl:w-[calc(1165px*0.205)]">
+    <div className="w-[20.5vw] py-[5px] tracking-[1px] xl:w-[calc(1165px*0.205)]">
       <div className="text-[12px] text-[#909090] leading-[14px]">{day}</div>
-      <div className="text-[14px] leading-[18px]">{activity}</div>
+      <div className="text-[14px] text-white leading-[18px]">{activity}</div>
     </div>
   );
 }
