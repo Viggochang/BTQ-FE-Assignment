8b9389e3e0cc92fc01ee8ba100f73b28d8c90dcf
Viggochang <gl4cj8686@gmail.com>
2021-12-28 05:30:32 +0800

    Use custom class name in conference intro

--- src/components/conference/Intro.js
+++ src/components/conference/Intro.js
@@ -4,10 +4,8 @@ import { conferenceData } from "../../data/data";
 
 export default function Intro() {
   return (
-    <div className="max-w-[560px] px-[3%] pb-[48px] md:px-0 lg:max-w-[815.5px] lg:w-[67.4vw] lg:pb-[20px]">
-      <p className="text-[15px] text-[#ff9fac] leading-[21px] text-center font-light xs:text-[17px] xs:leading-[26px] md:leading-[25px] lg:text-[21px] lg:leading-[32px]">
-        {conferenceData.intro}
-      </p>
+    <div className="intro-background">
+      <p className="intro-text text-[#ff9fac]">{conferenceData.intro}</p>
     </div>
   );
 }
