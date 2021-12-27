addf8262ab78c13a99559e8a900bdb166e8117f7
Viggochang <gl4cj8686@gmail.com>
2021-12-27 22:57:57 +0800

    Edit navIcon for mobile header

--- src/components/header/mobileHeader/navigation/NavIcon.js
+++ src/components/header/mobileHeader/navigation/NavIcon.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function NavIcon() {
-  return <div></div>;
+  return (
+    <div className="flex w-[18px] h-[18px] mt-[16px] mr-[17px] flex-col">
+      <div className="w-full h-[3px] bg-white mb-[3px] rounded-[3px]" />
+      <div className="w-full h-[3px] bg-white mb-[3px] rounded-[3px]" />
+      <div className="w-full h-[3px] bg-white mb-[3px] rounded-[3px]" />
+    </div>
+  );
 }
