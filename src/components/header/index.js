427e86a4dc2cce84f160ccd175036a16d5104da9
Viggochang <gl4cj8686@gmail.com>
2021-12-27 19:48:52 +0800

    Edit RWD for container in header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="w-full h-[156px] py-[30px] bg-my-black">
-      <div class="container"></div>
+    <header className="flex w-full h-[156px] py-[30px] bg-my-black">
+      <div class="xl:container xl:mx-auto w-full">
+        <div
+          className="w-1/5 h-[90px] bg-no-repeat bg-cover bg-center"
+          style={{
+            "background-image":
+              "url(https://2012.buildconf.com/images/logo-knockout@2x.png)",
+          }}
+        />
+      </div>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
       lg: "1024px",
       xl: "1165px",
     },
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      "50%": "50%",
+    },
     colors: {
       "my-black": "#191818",
     },
