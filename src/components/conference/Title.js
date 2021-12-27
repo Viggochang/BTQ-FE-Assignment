8fe4a3971a85c033512d85763a8138ed138bbb81
Viggochang <gl4cj8686@gmail.com>
2021-12-28 00:57:38 +0800

    Edit title background

--- src/components/conference/Title.js
+++ src/components/conference/Title.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Title() {
-  return <div></div>;
+  return (
+    <div
+      className="bg-[length:54px_11px] bg-no-repeat bg-bottom "
+      style={{
+        "background-image":
+          "url(https://2012.buildconf.com/images/nuts_red@2x.png)",
+      }}
+    ></div>
+  );
 }
--- src/components/conference/index.js
+++ src/components/conference/index.js
@@ -3,7 +3,7 @@ import Title from "./Title";
 
 export default function Conference() {
   return (
-    <section className="w-full px-[4vw] bg-primary">
+    <section className="w-full px-[4vw] pt-[54px] bg-primary">
       <Title />
     </section>
   );
