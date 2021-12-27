132d56a2e81c50aab143ed64fd6feb252c8886ab
Viggochang <gl4cj8686@gmail.com>
2021-12-28 04:09:48 +0800

    Modify speaker name style

--- src/components/conference/speakers/Speaker.js
+++ src/components/conference/speakers/Speaker.js
@@ -7,9 +7,9 @@ export default function Speaker({ data }) {
       <img
         alt={name}
         src={img}
-        className="w-[219px] md:w-[20.15vw] md:max-w-[219px] "
+        className="w-[219px] md:w-[20.15vw] md:max-w-[219px]"
       />
-      <h3 className="pt-[10px] text-[18px] text-[#ff9fac] leading-[21px] text-center">
+      <h3 className="w-[219px] pt-[10px] text-[18px] text-[#ff9fac] leading-[21px] text-center md:w-[20.15vw] md:max-w-[219px]">
         {name}
       </h3>
     </div>
