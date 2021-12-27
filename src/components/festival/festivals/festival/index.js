ecc0ead87b066cf2be334fe064840ca1866e3bb1
Viggochang <gl4cj8686@gmail.com>
2021-12-28 07:22:41 +0800

    Edit festival background style

--- src/components/festival/festivals/festival/index.js
+++ src/components/festival/festivals/festival/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Festival() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col items-center w-full mb-[26px] sm:w-[46%] md:w-[22%]"></div>
+  );
 }
