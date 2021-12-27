d3e30ac5585a12eba83b937938cf40323a710eff
Viggochang <gl4cj8686@gmail.com>
2021-12-28 07:02:10 +0800

    Edit Image

--- src/components/workshops/workshops/workshop/index.js
+++ src/components/workshops/workshops/workshop/index.js
@@ -1,17 +1,11 @@
 import React from "react";
+import Image from "./Image";
 import Info from "./Info";
 
 export default function Workshop({ data }) {
-  const { title, subtitle, intro, img } = data;
   return (
     <div className="flex flex-col items-center w-full mb-[26px] sm:w-[46%] md:w-[22%]">
-      <div className="flex w-full h-[226px] justify-center bg-primary rounded-[4px] md:h-auto md:pb-[10px]">
-        <img
-          alt={title}
-          src={img}
-          className="w-[219px] md:w-[20.15vw] md:max-w-[219px] rounded-[4px] self-center"
-        />
-      </div>
+      <Image data={data} />
       <Info data={data} />
     </div>
   );
