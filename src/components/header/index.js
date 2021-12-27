7d3b2967986b8e8c90e879095ec0d8920a48c2f7
Viggochang <gl4cj8686@gmail.com>
2021-12-27 21:07:41 +0800

    Edit the size for items in navigation

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ export default function Header() {
     <header className="flex w-full h-[156px] py-[30px] bg-my-black">
       <div class="xl:container xl:mx-auto w-full">
         <div
-          className="w-[20.5%] h-[90px] bg-no-repeat bg-cover bg-center absolute"
+          className="w-[20.5%] h-[90px] bg-no-repeat bg-cover bg-center absolute xl:w-[calc(1165px*0.205)]"
           style={{
             "background-image":
               "url(https://2012.buildconf.com/images/logo-knockout@2x.png)",
