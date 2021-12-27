df2c4d07275ddc2110487ec04736022a2271275f
Viggochang <gl4cj8686@gmail.com>
2021-12-28 07:13:07 +0800

    Init Title and Intro for festival

--- src/components/festival/index.js
+++ src/components/festival/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Title from "./Title";
+import Intro from "./Intro";
 
 export default function Festival() {
   return (
     <section className="section-background">
-      <div class="section-container"></div>
+      <div class="section-container">
+        <Title />
+        <Intro />
+      </div>
     </section>
   );
 }
