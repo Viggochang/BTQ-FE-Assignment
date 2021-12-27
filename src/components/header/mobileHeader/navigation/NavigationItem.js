b8d48f8606d2d4b055c51a9f2f32b5a359d16047
Viggochang <gl4cj8686@gmail.com>
2021-12-27 23:39:35 +0800

    Edit nav text style

--- src/components/header/mobileHeader/navigation/NavigationItem.js
+++ src/components/header/mobileHeader/navigation/NavigationItem.js
@@ -5,7 +5,9 @@ export default function NavigationItem({ data }) {
   return (
     <div className="w-full h-[49px] border-t-[1px] py-[8px] pl-[10px] border-[#272727] border-solid tracking-[1px]">
       <div className="text-[9px] text-[#909090] leading-[14px]">{day}</div>
-      <div className="text-[12px] text-white leading-[18px]">{activity}</div>
+      <div className="text-[12px] text-white leading-[18px] font-bold">
+        {activity}
+      </div>
     </div>
   );
 }
--- src/components/header/webHeader/navigation/NavigationItem.js
+++ src/components/header/webHeader/navigation/NavigationItem.js
@@ -5,7 +5,9 @@ export default function NavigationItem({ data }) {
   return (
     <div className="w-[20.5vw] py-[5px] tracking-[1px] xl:w-[calc(1165px*0.205)]">
       <div className="text-[12px] text-[#909090] leading-[14px]">{day}</div>
-      <div className="text-[14px] text-white leading-[18px]">{activity}</div>
+      <div className="text-[14px] text-white font-bold leading-[18px]">
+        {activity}
+      </div>
     </div>
   );
 }
