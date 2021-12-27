9f54f148165c64011e1173a6ab42b5d36f3edc16
Viggochang <gl4cj8686@gmail.com>
2021-12-28 05:17:20 +0800

    Init Intro for workshops

--- src/components/workshops/index.js
+++ src/components/workshops/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Title from "./Title";
+import Intro from "./Intro";
 
 export default function Workshops() {
   return (
     <section className="section-background">
       <div class="section-container">
         <Title />
+        <Intro />
       </div>
     </section>
   );
