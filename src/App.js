41f3a13f9285a77aa176960d51ca9d6e6545a0b4
Viggochang <gl4cj8686@gmail.com>
2021-12-28 00:03:40 +0800

    Init pattern

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/header";
+import Pattern from "./components/pattern";
 
 function App() {
   return (
     <div className="App">
       {/* <div className="text-5xl font-bold underline">Hello world!</div> */}
       <Header />
+      <Pattern />
     </div>
   );
 }
