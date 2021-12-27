df2663a90fc415230b2c316ec1c1c10c9ffa9011
Viggochang <gl4cj8686@gmail.com>
2021-12-28 07:16:26 +0800

    Edit intro

--- src/components/festival/Intro.js
+++ src/components/festival/Intro.js
@@ -1,5 +1,10 @@
 import React from "react";
+import { festivalData } from "../../data/data";
 
 export default function Intro() {
-  return <div></div>;
+  return (
+    <div className="intro-background">
+      <p className="intro-text text-[#909090]">{festivalData.intro}</p>
+    </div>
+  );
 }
