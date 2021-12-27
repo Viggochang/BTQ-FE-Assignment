b24be778f2d8e7a0ec0fa7aedd9d580f44f8f03f
Viggochang <gl4cj8686@gmail.com>
2021-12-28 05:56:54 +0800

    Edit workshop image

--- src/components/workshops/workshops/Workshop.js
+++ src/components/workshops/workshops/Workshop.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function Workshop() {
-  return <div></div>;
+export default function Workshop({ data }) {
+  const { title, subtitle, intro, img } = data;
+  return (
+    <div className="section-main">
+      <div className="flex w-full h-[226px] bg-primary">
+        <img
+          alt={title}
+          src={img}
+          className="w-[219px] mx-auto md:w-[20.15vw] md:max-w-[219px]"
+        />
+      </div>
+    </div>
+  );
 }
