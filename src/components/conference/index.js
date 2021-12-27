60ab682875e8e92472676b53314d65dcfcee0e2c
Viggochang <gl4cj8686@gmail.com>
2021-12-28 01:06:54 +0800

    Init Intro

--- src/components/conference/index.js
+++ src/components/conference/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Title from "./Title";
 import DateLoc from "./DateLoc";
+import Intro from "./Intro";
 
 export default function Conference() {
   return (
     <section className="w-full px-[4vw] pt-[54px] bg-primary">
       <Title />
       <DateLoc />
+      <Intro />
     </section>
   );
 }
