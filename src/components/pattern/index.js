425a8f7b448c5a8b587412f93e643a8e681aa8e0
Viggochang <gl4cj8686@gmail.com>
2021-12-28 00:23:26 +0800

    Edit pattern style

--- src/components/pattern/index.js
+++ src/components/pattern/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Pattern() {
-  return <div></div>;
+  return (
+    <div
+      className="absolute top-[48px] w-full h-[18px] bg-[length:1424px_40px] bg-bottom bg-repeat-x z-10 md:h-[31px] md:top-[156px]"
+      style={{
+        "background-image":
+          "url(https://2012.buildconf.com/images/pattern@2x.png)",
+      }}
+    ></div>
+  );
 }
