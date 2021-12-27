a5d5a515c69e06c2ddd1f3c493e56054d0b647bf
Viggochang <gl4cj8686@gmail.com>
2021-12-28 07:21:25 +0800

    Init Festival

--- src/components/festival/festivals/index.js
+++ src/components/festival/festivals/index.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Festival from "./festival";
 
+import { festivalData } from "../../../data/data";
+
+const { festivals } = festivalData;
 export default function InnerFestivals() {
-  return <div></div>;
+  return (
+    <div className="section-main-group justify-around md:justify-between">
+      {festivals.map((data) => (
+        <Festival key={data.title} data={data} />
+      ))}
+    </div>
+  );
 }
