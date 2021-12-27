489ffa50ccdc5af8f4108b2d3a267e0fd4a39dbb
Viggochang <gl4cj8686@gmail.com>
2021-12-28 00:59:01 +0800

    Init DataLoc

---
+++ src/components/conference/DateLoc.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DateLoc() {
+  return <div></div>;
+}
--- src/components/conference/index.js
+++ src/components/conference/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Title from "./Title";
+import DateLoc from "./DateLoc";
 
 export default function Conference() {
   return (
     <section className="w-full px-[4vw] pt-[54px] bg-primary">
       <Title />
+      <DateLoc />
     </section>
   );
 }
