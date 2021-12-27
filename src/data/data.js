10e83419d9e97ac68f414a09548b9d3614e7fff3
Viggochang <gl4cj8686@gmail.com>
2021-12-28 05:24:42 +0800

    Set workshops data into data file

--- src/data/data.js
+++ src/data/data.js
@@ -34,3 +34,39 @@ export const conferenceData = {
     },
   ],
 };
+
+export const workshopsData = {
+  title: "Workshops",
+  intro:
+    "Disconnect and make something with your own hands. In this year’s workshops, you’ll learn a traditional craft from accomplished masters from around the world, then leave at the end of the day with what you’ve made.",
+  workshops: [
+    {
+      title: "Axe Restoration",
+      subtitle: "Best Made Company",
+      intro:
+        "Join Best Made Co. for a two-day workshop on axe restoration, maintenance and use. We’ll end with a communal campfire and more axe and timber lore.",
+      img: "https://2012.buildconf.com/images/bestmadeco.png",
+    },
+    {
+      title: "Leather Craft",
+      subtitle: "Billykirk",
+      intro:
+        "Brothers Kirk and Chris Bray will spend the day teaching you the ins and outs of working with leather as you cut, press, stitch, dye and brand your own wallet.",
+      img: "https://2012.buildconf.com/images/leathercraft.png",
+    },
+    {
+      title: "Chocolatiering",
+      subtitle: "Taza Chocolate",
+      intro:
+        "Founder Alex Whitmore will teach the intricacies of bean selection, the perfect roast, grind and temper—even designing and wrapping the perfect chocolate to take home.",
+      img: "https://2012.buildconf.com/images/chocolate.png",
+    },
+    {
+      title: "Coffee Roasting",
+      subtitle: "Blue Bottle Coffee",
+      intro:
+        "James Freeman will join us for a full-day workshop on what makes a great cup of coffee. You’ll leave with the knowhow of the perfect cup of coffee.",
+      img: "https://2012.buildconf.com/images/coffee.png",
+    },
+  ],
+};
