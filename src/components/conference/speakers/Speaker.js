6bab95af1199f6146ba16165bac16ee8d4ea6f11
Viggochang <gl4cj8686@gmail.com>
2021-12-28 03:05:58 +0800

    Edit speaker's name

--- src/components/conference/speakers/Speaker.js
+++ src/components/conference/speakers/Speaker.js
@@ -3,8 +3,11 @@ import React from "react";
 export default function Speaker({ data }) {
   const { name, img } = data;
   return (
-    <div className="">
+    <div className="mb-[26px]">
       <img alt={name} src={img} className="w-[219px] h-[219px]" />
+      <h3 className="pt-[10px] text-[18px] text-[#ff9fac] leading-[21px] text-center">
+        {name}
+      </h3>
     </div>
   );
 }
