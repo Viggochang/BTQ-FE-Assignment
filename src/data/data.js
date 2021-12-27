40029adc77ebfcc581a921a82ec6a3bffab28985
Viggochang <gl4cj8686@gmail.com>
2021-12-28 07:10:07 +0800

    Set festival data into data file

--- src/data/data.js
+++ src/data/data.js
@@ -70,3 +70,29 @@ export const workshopsData = {
     },
   ],
 };
+
+export const festivalData = {
+  title: "Festival",
+  intro:
+    "We’re just getting started. Build isn’t just a conference—it’s a week long festival of events. Here’s a taste of the rest of the week, with more to come.",
+  festivals: [
+    {
+      title: "Film",
+      intro:
+        "We’ll be screening excellent design-related films and documentaries, and keep the beer, popcorn, and pizza flowing as you curl up in the dark and enjoy some quality cinema with your fellow designers.",
+      img: "https://2012.buildconf.com/images/film_black.png",
+    },
+    {
+      title: "Practical",
+      intro:
+        "Work, rest, or play. We’ll also host a Pecha Kucha at our mini-theatre, where you’ll also be able to show your work and discuss ideas with other attendees, or just crack open a beer and play some Mario Kart. Full line-up to be announced.",
+      img: "https://2012.buildconf.com/images/practical.png",
+    },
+    {
+      title: "Music",
+      intro:
+        "It wouldn’t be a proper festival without quality live music. For the first time at Build, join us for a night of performances from some of the best bands around. We’re still confirming our lineup, so check back soon for full details.",
+      img: "https://2012.buildconf.com/images/music.png",
+    },
+  ],
+};
