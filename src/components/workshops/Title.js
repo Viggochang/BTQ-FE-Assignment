aac89bf8ac353cfcf1da5bd64a914684d9926f61
Viggochang <gl4cj8686@gmail.com>
2021-12-28 04:48:35 +0800

    Edit Title style and text for workshops

--- src/components/workshops/Title.js
+++ src/components/workshops/Title.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Title() {
-  return <div></div>;
+  return (
+    <div className="title-background">
+      <h2 className="title-text text-my-black">Workshops</h2>
+    </div>
+  );
 }
