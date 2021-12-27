1c604484fd923b5ad8279b74839cbc303038d54e
Viggochang <gl4cj8686@gmail.com>
2021-12-28 05:42:08 +0800

    Set custom class name for section main group

--- src/components/conference/speakers/index.js
+++ src/components/conference/speakers/index.js
@@ -6,7 +6,7 @@ import { conferenceData } from "../../../data/data";
 const { speakers } = conferenceData;
 export default function Speakers() {
   return (
-    <div className="flex flex-wrap justify-center px-[1.5%] md:px-0">
+    <div className="section-main-group">
       {speakers.map((data) => (
         <Speaker key={data.name} data={data} />
       ))}
