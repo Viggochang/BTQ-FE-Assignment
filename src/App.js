785949529afcbd953a20ab601dc75a8241b532d9
Viggochang <gl4cj8686@gmail.com>
2021-12-28 04:31:41 +0800

    Init Workshops

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import Pattern from "./components/pattern";
 import Conference from "./components/conference";
+import Workshops from "./components/workshops";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Pattern />
       <Conference />
+      <Workshops />
     </div>
   );
 }
