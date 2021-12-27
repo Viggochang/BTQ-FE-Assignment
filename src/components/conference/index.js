2e755b7530ab715e3894f75a3fba1ed044469c66
Viggochang <gl4cj8686@gmail.com>
2021-12-28 00:44:36 +0800

    Init Title in conference

--- src/components/conference/index.js
+++ src/components/conference/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Title from "./Title";
 
 export default function Conference() {
-  return <section className="w-full px-[4vw] bg-primary"></section>;
+  return (
+    <section className="w-full px-[4vw] bg-primary">
+      <Title />
+    </section>
+  );
 }
