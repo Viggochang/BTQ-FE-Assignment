cdd66d7669ac4f9b3ccb23eb8e8ab56f6a56d9ad
Viggochang <gl4cj8686@gmail.com>
2021-12-27 19:44:08 +0800

    Set RWD breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "520px",
+      md: "720px",
+      lg: "1024px",
+      xl: "1165px",
+    },
     colors: {
       "my-black": "#191818",
     },
