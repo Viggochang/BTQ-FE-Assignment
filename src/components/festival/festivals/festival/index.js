4ba72de0a64bef7de856beef8aec9615baf94698
Viggochang <gl4cj8686@gmail.com>
2021-12-28 07:24:07 +0800

    Init Info and Image for festival

--- src/components/festival/festivals/festival/index.js
+++ src/components/festival/festivals/festival/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Image from "./Image";
+import Info from "./Info";
 
-export default function Festival() {
+export default function Festival({ data }) {
   return (
-    <div className="flex flex-col items-center w-full mb-[26px] sm:w-[46%] md:w-[22%]"></div>
+    <div className="flex flex-col items-center w-full mb-[26px] sm:w-[46%] md:w-[22%]">
+      <Image data={data} />
+      <Info data={data} />
+    </div>
   );
 }
