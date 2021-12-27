9e860b6f0c9258700c6253a12301e20a08220c1d
Viggochang <gl4cj8686@gmail.com>
2021-12-28 04:57:24 +0800

    Set custom class name for section

--- src/components/conference/index.js
+++ src/components/conference/index.js
@@ -8,7 +8,7 @@ import Speakers from "./speakers";
 export default function Conference() {
   return (
     <section
-      className="flex w-full px-[4vw] pt-[54px] pb-[171px] bg-primary bg-[length:1500px_171px] bg-bottom bg-repeat-x md:pt-[75px] md:px-[20px] md:pb-[214px]"
+      className="section-background bg-primary bg-[length:1500px_171px] bg-bottom bg-repeat-x"
       style={{
         "background-image":
           "url(https://2012.buildconf.com/images/conf_bg@2x2.png)",
