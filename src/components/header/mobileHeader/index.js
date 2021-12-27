4cf2ed4eb839082c19bc55daaedca9bef340fffc
Viggochang <gl4cj8686@gmail.com>
2021-12-27 23:01:50 +0800

    Init navigation for mobile header

--- src/components/header/mobileHeader/index.js
+++ src/components/header/mobileHeader/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import NavIcon from "./navigation/NavIcon";
+import Navigation from "./navigation";
 
 export default function MobileHeader() {
   return (
-    <header className="flex w-full h-[48px] bg-my-black justify-between md:hidden">
-      <div
-        className="w-[44px] h-[44px] mb-auto mt-[1px] ml-[4px] bg-[length:238px_90px] bg-center"
-        style={{
-          "background-image":
-            "url(https://2012.buildconf.com/images/logo-knockout@2x.png)",
-        }}
-      />
-      <NavIcon />
-    </header>
+    <>
+      <header className="flex w-full h-[48px] bg-my-black justify-between md:hidden">
+        <div
+          className="w-[44px] h-[44px] mb-auto mt-[1px] ml-[4px] bg-[length:238px_90px] bg-center"
+          style={{
+            "background-image":
+              "url(https://2012.buildconf.com/images/logo-knockout@2x.png)",
+          }}
+        />
+        <NavIcon />
+      </header>
+      <Navigation />
+    </>
   );
 }
