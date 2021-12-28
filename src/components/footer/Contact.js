c177b8bdf6b23daccc250ae74724f693b0496fb3
Viggochang <gl4cj8686@gmail.com>
2021-12-28 14:56:21 +0800

    Modify tailwind classes

--- src/components/footer/Contact.js
+++ src/components/footer/Contact.js
@@ -3,11 +3,9 @@ import React from "react";
 export default function Contact({ data }) {
   const { title, subtitle, img } = data;
   return (
-    <div className="flex flex-col items-center w-[50%] mb-[25px] md:w-[22%]">
-      <h4 className="hidden sm:block mb-[2px] tracking-[1px] text-[12px] text-info-color leading-[21px] text-center uppercase">
-        {subtitle}
-      </h4>
-      <h3 className="text-[14px] mb-[15px] tracking-[1px] sm:text-[18px] text-contact-color leading-[21px] text-center font-light md:mb-[21px]">
+    <div className="inner-flex-box w-[50%] mb-[25px] md:w-[22%]">
+      <h4 className="inner-subtitle-text hidden sm:block">{subtitle}</h4>
+      <h3 className="inner-title-text text-[14px] sm:text-[18px] text-contact-color leading-[21px]">
         {title}
       </h3>
       <div
