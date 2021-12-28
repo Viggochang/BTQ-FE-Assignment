3aa3464b1a8b8efc4505244ef2a0d1919fae072e
Viggochang <gl4cj8686@gmail.com>
2021-12-28 12:18:59 +0800

    Edit image in footer

--- src/components/footer/Contact.js
+++ src/components/footer/Contact.js
@@ -1,15 +1,21 @@
 import React from "react";
 
 export default function Contact({ data }) {
-  const { title, subtitle, image } = data;
+  const { title, subtitle, img } = data;
   return (
     <div className="flex flex-col items-center w-[50%] mb-[25px] md:w-[22%]">
-      <h4 className="mb-[2px] tracking-[1px] text-[12px] text-[#999] leading-[21px] text-center uppercase">
+      <h4 className="hidden sm:block mb-[2px] tracking-[1px] text-[12px] text-[#999] leading-[21px] text-center uppercase">
         {subtitle}
       </h4>
-      <h3 className="mb-[15px] tracking-[1px] text-[18px] text-[#bbb] leading-[21px] text-center font-light md:mb-[21px]">
+      <h3 className="text-[14px] mb-[15px] tracking-[1px] sm:text-[18px] text-[#bbb] leading-[21px] text-center font-light md:mb-[21px]">
         {title}
       </h3>
+      <div
+        className="w-full h-[36px] bg-no-repeat bg-[length:36px_36px] bg-bottom"
+        style={{
+          backgroundImage: `url(${img})`,
+        }}
+      ></div>
     </div>
   );
 }
