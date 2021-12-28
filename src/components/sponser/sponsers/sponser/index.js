7a4ee70314200c8c2a0781329c807dcf52b60b65
Viggochang <gl4cj8686@gmail.com>
2021-12-28 12:56:45 +0800

    Modify margin in sponser

--- src/components/sponser/sponsers/sponser/index.js
+++ src/components/sponser/sponsers/sponser/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Sponser({ data }) {
   return (
-    <div className="flex flex-col items-center w-[50%] mb-[26px] pt-[10px] pb-[13px] md:w-[12.6%] md:pb-0 md:mx-[2%]">
+    <div className="flex flex-col items-center w-[50%] pt-[10px] pb-[13px] md:w-[12.6%] md:pb-0 md:mx-[2%]">
       <img
         alt={data.name}
         src={data.img}
