0761ba796abbef52c336da6c770653fbd346e15a
Viggochang <gl4cj8686@gmail.com>
2021-12-28 13:03:42 +0800

    Edit credit text

--- src/components/credit/index.js
+++ src/components/credit/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Credit() {
-  return <div></div>;
+  return (
+    <div className="py-[12.5px]">
+      <div className="text-[12px] text-[#4a4a4a] leading-[25px]">
+        © 2012 Andy McMillan / Design by Kyle Meyer / Development by Riot /
+        Icons by Tim Boelaars / Refund Policy
+      </div>
+    </div>
+  );
 }
