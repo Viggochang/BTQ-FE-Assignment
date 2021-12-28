73a3a67dad6ffaebe08bb16f9c08128a52e8e323
Viggochang <gl4cj8686@gmail.com>
2021-12-28 08:28:04 +0800

    Init Misc

--- src/components/misc/miscs/index.js
+++ src/components/misc/miscs/index.js
@@ -1,5 +1,12 @@
 import React from "react";
+import Misc from "./misc";
 
 export default function InnerMiscs({ data }) {
-  return <div></div>;
+  return (
+    <div className="section-main-group justify-around md:justify-between">
+      {data.map((data) => (
+        <Misc key={data.title} data={data} />
+      ))}
+    </div>
+  );
 }
