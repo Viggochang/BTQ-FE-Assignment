d955d6198a2da8a63d9f61b1202cfe346ea819a9
Viggochang <gl4cj8686@gmail.com>
2021-12-28 11:56:11 +0800

    Contect

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Contact from "./contact";
 
 export default function Footer({ data }) {
   return (
     <footer className="footer-background">
-      <div className="section-container"></div>
+      <div className="section-container">
+        <div className="section-main-group justify-around md:justify-between">
+          {data.map((data) => (
+            <Contact key={data.title} />
+          ))}
+        </div>
+      </div>
     </footer>
   );
 }
