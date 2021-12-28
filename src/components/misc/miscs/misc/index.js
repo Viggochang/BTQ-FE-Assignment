9aa905b79ab6fee6d6d3d77a6fa407f91ff55568
Viggochang <gl4cj8686@gmail.com>
2021-12-28 08:30:37 +0800

    Init INfo

--- src/components/misc/miscs/misc/index.js
+++ src/components/misc/miscs/misc/index.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Info from "./Info";
 
-export default function Misc() {
-  return <div></div>;
+export default function Misc({ data }) {
+  return (
+    <div className="flex flex-col items-center w-full mb-[26px] sm:w-[46%]">
+      <Info data={data} />
+    </div>
+  );
 }
