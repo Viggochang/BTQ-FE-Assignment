eb6b4221e65f940f96656fe5231e2d8f15713116
Viggochang <gl4cj8686@gmail.com>
2021-12-28 08:20:37 +0800

    Set misc data into data file

--- src/data/data.js
+++ src/data/data.js
@@ -132,3 +132,18 @@ export const fringeData = {
     },
   ],
 };
+
+export const miscData = [
+  {
+    title: "Draft Supply Co.",
+    subtitle: "Pop-Up Shop",
+    intro:
+      "New to Build in 2012 is the Draft Supply Co. pop-up shop, stocking quality goods for the discerning digital craftsman. Low on stationary, need a gift, or feel like picking up some Build swag? ",
+  },
+  {
+    title: "The Black Box",
+    subtitle: "Pop-Up Cafe",
+    intro:
+      "We’ll be establishing a base of operations at The Black Box on Hill Street, right at the epicentre of the Build festivities. Our pop-up cafe will serve a full menu all day, offer free wifi, and will be staffed by Build volunteers who’ll be constantly on hand to answer any questions.",
+  },
+];
