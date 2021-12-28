4a011425a890f1257fae40d2759b4b77fcb2089b
Viggochang <gl4cj8686@gmail.com>
2021-12-28 13:11:24 +0800

    Edit text style

--- src/components/credit/index.js
+++ src/components/credit/index.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Credit() {
   return (
-    <div className="py-[12.5px]">
-      <div className="text-[12px] text-[#4a4a4a] leading-[25px]">
+    <div className="py-[12.5px] bg-my-black">
+      <div className="text-[14px] text-[#4a4a4a] leading-[25px] font-light text-center">
         © 2012 Andy McMillan / Design by Kyle Meyer / Development by Riot /
         Icons by Tim Boelaars / Refund Policy
       </div>
