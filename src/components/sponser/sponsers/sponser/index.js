c91c2b9a74c870bed1389d39ad7fb733bf832e93
Viggochang <gl4cj8686@gmail.com>
2021-12-28 09:14:23 +0800

    Edit Sponser

--- src/components/sponser/sponsers/sponser/index.js
+++ src/components/sponser/sponsers/sponser/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Sponser() {
-  return <div></div>;
+export default function Sponser({ data }) {
+  return (
+    <div className="flex flex-col items-center w-[50%] mb-[26px] pt-[10px] pb-[13px] md:w-[12.6%] md:pb-0 md:mx-[2%]">
+      <img
+        alt={data.name}
+        src={data.img}
+        className="w-[100%] max-w-[60%] md:max-w-[219px] rounded-[4px] self-center"
+      />
+    </div>
+  );
 }
