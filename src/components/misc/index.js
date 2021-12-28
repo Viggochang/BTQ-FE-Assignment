eba41a5fb8feff1f6bfe651f561f768dfcb2fb6d
Viggochang <gl4cj8686@gmail.com>
2021-12-28 08:26:53 +0800

    Init InnerMisc

--- src/components/misc/index.js
+++ src/components/misc/index.js
@@ -1,9 +1,12 @@
 import React from "react";
+import InnerMiscs from "./miscs";
 
-export default function Misc({ data }) {
+export default function Miscs({ data }) {
   return (
     <section className="section-background">
-      <div className="section-container"></div>
+      <div className="section-container">
+        <InnerMiscs data={data} />
+      </div>
     </section>
   );
 }
